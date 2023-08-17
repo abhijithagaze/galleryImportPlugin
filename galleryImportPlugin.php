eddba8a27e08f30050e02ca11e14e55bf202e302
abhijithagze <abhijith@agaze.in>
2023-08-17 19:35:09 +0530

    fetchImgfrmMedialib

--- galleryImportPlugin.php
+++ galleryImportPlugin.php
@@ -5,7 +5,9 @@
     Description: Add images into product gallery from S3 bucket.
     Version: 1.0
     Author: Abhijith  Santhosh
+
     */
+
     // Step 1: Create the Settings Page
     function image_uploader_plugin_settings_page()
     {
@@ -18,58 +20,140 @@
             'image_uploader_plugin_settings_callback'
         );
     }
+
     function image_uploader_plugin_settings_callback()
     {
         // Check if the user has permissions to access the settings page
         if (!current_user_can('manage_options')) {
             return;
         }
-        // Save the URL in the database when the form is submitted
-        if (isset($_POST['image_uploader_url'])) {
-            save_image_uploader_url();
-        }
+
+        // // Save the URL in the database when the form is submitted
+        // if (isset($_POST['image_uploader_url'])) {
+        //     save_image_uploader_url();
+        // }
+
         // Handle the image import on button click
-        if (isset($_POST['import_images'])) {
+        if (isset($_POST['sku_without_img_button'])) {
             fetch_and_attach_images();
         }
+        if (isset($_POST['import_all_button'])) {
+          
+        }
+        if (isset($_POST['view-button'])) {
+            fetch_images_from_medialibrary();
+        }
+
         // Display the settings page form
     ?>
-        <div class="wrap">
-            <h1>Set Your URL Here</h1>
-            <form method="post" action="">
-                <table class="form-table">
-                    <tr>
-                        <th scope="row"><label for="image_uploader_url">Enter cloud URL:</label></th>
-                        <td>
-                            <input type="text" name="image_uploader_url" id="image_uploader_url" value="<?php echo esc_attr(get_option('image_uploader_url')); ?>">
-                            <p class="description">Enter the cloud  URL where the images are stored.</p>
-                        </td>
-                    </tr>
-                </table>
-                <?php submit_button('Save URL'); ?>
-            </form>
-            <!-- Add the Import button -->
-            <form method="post" action="">
-                <?php
-                // Nonce field for security
-                wp_nonce_field('import_images_action', 'import_images_nonce');
-                ?>
-                <input type="hidden" name="import_images" value="1">
-                <?php submit_button('Import Images', 'primary', 'import_images_button'); ?>
-            </form>
-        </div>
+       <div class='section section--import section--import--images' style="background-color: #f7f7f7; padding: 20px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); display: flex; justify-content: space-between; align-items: center;">
+    <h2 style="margin-bottom: 10px;">Import Product Images</h2>
+    <div class='section--content' style="position: relative;">
+        <form method="post" action="">
+            <?php
+            wp_nonce_field('import_images_action2','import_images_nonce2');
+            ?>
+            <input type="hidden" name="sku_without_img_button" valu="1">
+            <?php submit_button('CLICK HERE TO GET PRODUCTS SKU WITHOUT IMAGE GALLERY','Primary','import_images_button2'); ?>
+        </form>
+        <!-- <button id="sku_without_img" style="background-color: #4CAF50; color: white; padding: 5px 10px; border: none; border-radius: 5px; cursor: pointer; transition: background-color 0.3s, transform 0.2s;">
+           CLICK HERE TO GET PRODUCTS SKU WITHOUT IMAGE GALLERY
+        </button> -->
+        <!-- Dropdown menu to display fetched content -->
+        <select id="contentDropdown" style="position: absolute; top: 100%; left: 0; margin-top: 5px; display: none;">
+            <option value="">Select content</option>
+        </select>
+    </div>
+    <form method="post" action="">
+        <?php
+        wp_nonce_field('import_images_action','import_images_nonce');
+        ?>
+        <input type="hidden" name="import_all_button" valu="1">
+        <?php submit_button('Import All','Primary','import_images_button'); ?>
+    </form>
+    <!-- <button id="import-all-button" style="background-color: #FF9800; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; transition: background-color 0.3s, transform 0.2s;">
+        IMPORT ALL
+    </button>
+     -->
+</div>
+
+<!-- New section for entering SKU to import image -->
+<!-- <div class='section section--import' style="background-color: #f7f7f7; padding: 20px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); display: flex; justify-content: space-between; align-items: center; margin-top: 20px;">
+    <label for="skuInput" style="font-weight: bold;">ENTER SKU TO IMPORT IMAGE:</label>
+    <div>
+        <input type="text" id="skuInput" style="padding: 10px; border: 5px solid #ccc; border-radius: 3px; margin-right: 763px;">
+        <button id="viewButton" style="background-color: #3498db; color: white; padding: 5px 10px; border: none; border-radius: 30px; cursor: pointer; transition: background-color 0.3s, transform 0.2s;">VIEW</button>
+    </div>
+</div> -->
+
+<div class='section section--import' style="background-color: #f7f7f7; padding: 20px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); display: flex; justify-content: space-between; align-items: center; margin-top: 20px;">
+    <label for="skuInput" style="font-weight: bold;">ENTER SKU/Product ID TO VIEW GALLERY:</label>
+    <div>
+        <input type="text" id="product_id" style="padding: 10px; border: 5px solid #ccc; border-radius: 3px; margin-right: 763px;">
+        <form method="post" action="">
+            <?php
+            wp_nonce_field('import_images_action2','import_images_nonce2');
+            ?>
+            <input type="hidden" name="view_button" valu="1">
+            <?php submit_button('viewButton','Primary','import_images_button3'); ?>
+        </form>
+        <!-- <button id="viewButton" style="background-color: #3498db; color: white; padding: 5px 10px; border: none; border-radius: 30px; cursor: pointer; transition: background-color 0.3s, transform 0.2s;">VIEW</button> -->
+    </div>
+</div>
+
+<!-- <script>
+    // Function to fetch and populate the dropdown with content
+    function fetchContent() {
+        // Assuming you have a PHP function that fetches content
+        // and returns it as a JSON response
+        // Example PHP function: fetch_content_from_external_source()
+        
+        // You can use AJAX to fetch content from the server
+        // Here, we're using a dummy response for demonstration
+        const dummyResponse = [
+            { value: 'item1', label: 'Item 1' },
+            { value: 'item2', label: 'Item 2' },
+            { value: 'item3', label: 'Item 3' }
+        ];
+        
+        const contentDropdown = document.getElementById('contentDropdown');
+        
+        // Clear existing options
+        contentDropdown.innerHTML = '<option value="">Select content</option>';
+        
+        // Populate dropdown with fetched content
+        dummyResponse.forEach(item => {
+            const option = document.createElement('option');
+            option.value = item.value;
+            option.textContent = item.label;
+            contentDropdown.appendChild(option);
+        });
+        
+        // Display the dropdown
+        contentDropdown.style.display = 'block';
+    }
+    
+    // Attach click event handler to the button
+    const sku_without_img = document.getElementById('sku_without_img');
+    sku_without_img.addEventListener('click', fetchContent);
+    
+    // Function to handle "View" button click
+    // const viewButton = document.getElementById('viewButton');
+    // viewButton.addEventListener('click', function () {
+    //     const skuInput = document.getElementById('skuInput').value;
+    //     console.log('SKU to import image:', skuInput);
+    //     // You can perform the necessary action with the SKU here
+    // });
+</script> -->
     <?php
     }
-    // Step 2: Save the URL in the Database
-    function save_image_uploader_url()
-    {
-        // Handle the uploaded S3 bucket URL here
-        $cloud_url = sanitize_text_field($_POST['image_uploader_url']);
-        update_option('image_uploader_url', $cloud_url);
-        echo '<div class="notice notice-success"><p>URL successfully saved!</p></div>';
-    }
+
+
     // Hook the settings page function to the admin menu
     add_action('admin_menu', 'image_uploader_plugin_settings_page');
+
+
+
     // Step 3: Retrieve Product IDs
     function get_product_ids()
     {
@@ -78,68 +162,73 @@
             'post_type' => 'product',
             'posts_per_page' => -1,
         ));
+
         $product_ids = array();
         foreach ($products as $product) {
             $product_ids[] = $product->ID;
         }
+
         return $product_ids;
     }
-  
-  
-    // Function to fetch and attach images from SharePoint
-function fetch_and_attach_images() {
-    $sharepoint_url = 'https://16sknv.sharepoint.com/';
-    $access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvMTZza252LnNoYXJlcG9pbnQuY29tQDkxMTY1ZDQ0LTRjZDktNDJmYy05ZGRlLWIyYzgzNzdmMzRhZiIsImlzcyI6IjAwMDAwMDAxLTAwMDAtMDAwMC1jMDAwLTAwMDAwMDAwMDAwMEA5MTE2NWQ0NC00Y2Q5LTQyZmMtOWRkZS1iMmM4Mzc3ZjM0YWYiLCJpYXQiOjE2OTE5ODg2NjgsIm5iZiI6MTY5MTk4ODY2OCwiZXhwIjoxNjkyMDc1MzY4LCJpZGVudGl0eXByb3ZpZGVyIjoiMDAwMDAwMDEtMDAwMC0wMDAwLWMwMDAtMDAwMDAwMDAwMDAwQDkxMTY1ZDQ0LTRjZDktNDJmYy05ZGRlLWIyYzgzNzdmMzRhZiIsIm5hbWVpZCI6ImRmMjBjODg1LTM5OWYtNDg0YS05Mjk4LWYyNDE4ODI4YzI1YkA5MTE2NWQ0NC00Y2Q5LTQyZmMtOWRkZS1iMmM4Mzc3ZjM0YWYiLCJvaWQiOiJjM2MyZDMyZC04NzczLTRiMzMtODU1Yy1hNzI0ZmNkNzA3Y2EiLCJzdWIiOiJjM2MyZDMyZC04NzczLTRiMzMtODU1Yy1hNzI0ZmNkNzA3Y2EiLCJ0cnVzdGVkZm9yZGVsZWdhdGlvbiI6ImZhbHNlIn0.TL99KYr3nPD6LXgyXX1W1IfhROyNSlsPY3ZldHO7XGQxtTBZ4g__YXuG80fgM5NbE-KwDRtzcU7ssE3wxsjOLcV1NTHCL3A35zQ97Ps0YitvlnziOKGBn2B_hiJTa-YH1UJS31arnPIj69pOFXPZYwESvvd-6lLbE--AptkWWmM55-VkTN36wz12fjuLEEAKlPUvLHG6foOOqMl5aPI-IIR_qDgSjl69WmLPXPj-g9h64Ask4Ova_1x24i_-nRd4TCEzAvNIu1zmKGNV4D_ShxPvE_tS3Y93En1WkkbDnIan5SV9y2hCV3i1Hb8FTodnf2lUPTwOifF8KIMHDZSpkQ'; // Replace with your access token
+    // Step 4: Fetch Images and Attach to Products
+    function fetch_and_attach_images()
+    {
+        // $s3_bucket_url = get_option('image_uploader_url');
+        // // Check if the S3 bucket URL is set
+        // if (empty($s3_bucket_url)) {
+        //     return;
+        // }
+        // $product_ids = get_product_ids();
+        // foreach ($product_ids as $product_id) {
+        //     $image_filename = $product_id . '.jpg'; // Assuming the image filename is the same as the product ID and has a .jpg extension
+        //     // Construct the URL of the image in the S3 bucket
+        //     $image_url = trailingslashit($s3_bucket_url) . $image_filename;
+        
+        //    print_r("fetch attach function called")
+            // attach_images($image_url, $product_id);
+    }
 
-    $product_ids = get_product_ids();
-
-    foreach ($product_ids as $product_id) {
-        // $image_filename = $product_id . '.jpg';
-        $item_id = $product_id; // Assuming item IDs match product IDs
-
-        $image_url = construct_sharepoint_image_url($sharepoint_url, $item_id, $access_token);
-
-        if ($image_url) {
-            attach_images($image_url, $product_id);
+    function fetch_images_from_medialibrary(){
+        if (isset($_POST['product_id'])) {
+            $product_id = intval($_POST['product_id']);
+            print_r($product_id);
+            
+            // Fetch product by ID
+            $product = wc_get_product($product_id);
+            print_r($product);
+            
+            if ($product) {
+                // Get product gallery images
+                $gallery_ids = $product->get_gallery_image_ids();
+                print_r($gallery_ids);
+                if (!empty($gallery_ids)) {
+                    foreach ($gallery_ids as $image_id) {
+                        $image_url = wp_get_attachment_image_url($image_id, 'full');
+                        echo '<img src="' . esc_url($image_url) . '" alt="Product Image">';
+                    }
+                } else {
+                    echo 'No gallery images found for the specified product.';
+                }
+            } else {
+                echo 'Product not found with the specified ID.';
+            }
         }
     }
-}
-
-// Function to construct SharePoint image URL
-function construct_sharepoint_image_url($sharepoint_url, $item_id, $access_token) {
-    $api_endpoint = "https://16sknv.sharepoint.com/_api/web/GetFolderByServerRelativeUrl('/gallery_1')/Files";
     
-    $headers = array(
-        'Authorization' => 'Bearer ' . $access_token,
-    );
-
-    $response = wp_safe_remote_get($api_endpoint, array('headers' => $headers));
-
-    if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) {
-        $data = wp_remote_retrieve_body($response);
-        $json_data = json_decode($data);
-
-        if (isset($json_data->ServerRelativeUrl)) {
-            $image_url = "$sharepoint_url" . $json_data->ServerRelativeUrl;
-            return $image_url;
+    
+    function attach_images($image_url, $product_id)
+    {
+        // Upload the image to the WordPress media library
+        $attachment_id = media_sideload_image($image_url,null,null,'id');
+         if (is_wp_error($attachment_id)) {
+            // There was an error uploading the image
+            echo 'Error uploading image: ' . $attachment_id->get_error_message();
+            } else {
+            // Image uploaded successfully, and $attachment_id contains the attachment ID
+            echo 'Image uploaded successfully! Attachment ID: ' . $attachment_id;
+            // Append the attachment ID to the product gallery
+            $product_gallery = get_post_meta($product_id, '_product_image_gallery', true);
+            $product_gallery .= ',' . $attachment_id;
+            update_post_meta($product_id, '_product_image_gallery', $product_gallery);
         }
-    }       
-
-    // return null;
-}
-function attach_images($image_url, $product_id)
-{
-    // Upload the image to the WordPress media library
-    $attachment_id = media_sideload_image($image_url,null,null,'id');
-     if (is_wp_error($attachment_id)) {
-        // There was an error uploading the image
-        echo 'Error uploading image: ' . $attachment_id->get_error_message();
-        } else {
-        // Image uploaded successfully, and $attachment_id contains the attachment ID
-        echo 'Image uploaded successfully! Attachment ID: ' . $attachment_id;
-        // Append the attachment ID to the product gallery
-        $product_gallery = get_post_meta($product_id, '_product_image_gallery', true);
-        $product_gallery .= ',' . $attachment_id;
-        update_post_meta($product_id, '_product_image_gallery', $product_gallery);
     }
-}
